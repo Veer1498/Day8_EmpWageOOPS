6d999cdc8e44aac23828ba4ec62720e2a17bf78f
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-30 00:26:44 +0530

    Check Whether Employee is present or Absent

--- src/oopsproblems/EmpWageObject.java
+++ src/oopsproblems/EmpWageObject.java
@@ -5,7 +5,12 @@ public class EmpWageObject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
-
+		int employeePresent = 1;
+		int isPresent = (int) Math.floor(Math.random()*10) %2;
+		if (isPresent == employeePresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
